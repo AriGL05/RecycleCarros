ef94227356585a133620b4e83e4144c99a4c2850
Carlos Avalos Soto <carlos.avalos0409@gmail.com>
2023-10-25 13:38:01 -0600

    Adapter created with view

--- app/src/main/java/com/example/recyclecarros/adapters/CarroAdapter.java
+++ app/src/main/java/com/example/recyclecarros/adapters/CarroAdapter.java
@@ -1,11 +1,14 @@
 package com.example.recyclecarros.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.recyclecarros.R;
 import com.example.recyclecarros.models.Carro;
 
 import java.util.List;
@@ -20,22 +23,35 @@ public class CarroAdapter extends RecyclerView.Adapter<CarroAdapter.CarroHolder>
     @NonNull
     @Override
     public CarroAdapter.CarroHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater ly=LayoutInflater.from(parent.getContext());
+        View view=ly.inflate(R.layout.itemscarro,parent,false);
+        return new CarroHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull CarroAdapter.CarroHolder holder, int position) {
-
+        Carro c=listaCarros.get(position);
+        holder.setdata(c);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listaCarros.size();
     }
     public class CarroHolder extends RecyclerView.ViewHolder{
-
+        TextView tvcolor;
+        TextView tvmodelo;
+        Carro carro;
         public CarroHolder(@NonNull View itemView) {
             super(itemView);
+            tvcolor=itemView.findViewById(R.id.tvColor);
+            tvmodelo=itemView.findViewById(R.id.tvModelo);
+        }
+
+        public void setdata(Carro c) {
+            carro=c;
+            tvcolor.setText(c.getColor());
+            tvmodelo.setText(c.getModelo());
         }
     }
 }
